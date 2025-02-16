4d35e931a95235e56878608d15ac66010c5231be
KostX3 <xgogohogox3@gmail.com>
2025-02-16 15:32:36 +0200

    Add files via upload

--- maze/main.py
+++ maze/main.py
@@ -1,5 +1,15 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+  def __init__(self,picture,w,h,x,y): 
+    super().__init__()
+    self.image=transform.scale(image.load(picture),(w, h))
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+
+  def reset(self):
+    window.blit(self.image,(self.rect.x,self.rect.y))
 
 window_width = 700
 window_height = 500
@@ -8,8 +18,13 @@ window = display.set_mode((window_width, window_height))
 
 run = True
 
+wall_1 = GameSprite('assets/wall.png',80,180,200,250)
+
 while run:
     for e in event.get():
+        window.fill((0, 200, 255))
         if e.type == QUIT:
             run = False
+            
+    wall_1.reset()
     display.update()
