6160461a66201b158eb7e99970d95b972aacd5b1
KostX3 <xgogohogox3@gmail.com>
2025-02-23 15:40:59 +0200

    Add files via upload

--- maze/main.py
+++ maze/main.py
@@ -1,30 +1,76 @@
 from pygame import *
-
+#клас-батько для інших спрайтів
 class GameSprite(sprite.Sprite):
-  def __init__(self,picture,w,h,x,y): 
-    super().__init__()
-    self.image=transform.scale(image.load(picture),(w, h))
-    self.rect = self.image.get_rect()
-    self.rect.x = x
-    self.rect.y = y
+#конструктор класу
+   def __init__(self, player_image, player_x, player_y, size_x, size_y):
+    # Викликаємо конструктор класу (Sprite):
+       sprite.Sprite.__init__(self)
+    #кожен спрайт повинен зберігати властивість image - зображення
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+    #кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+#метод, що малює героя на вікні
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+#метод, у якому реалізовано управління спрайтом за кнопками стрілочкам клавіатури
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_x_speed,player_y_speed):
+    # Викликаємо конструктор класу (Sprite):
+       GameSprite.__init__(self, player_image, player_x, player_y, size_x, size_y)
+       self.x_speed = player_x_speed
+       self.y_speed = player_y_speed
 
-  def reset(self):
-    window.blit(self.image,(self.rect.x,self.rect.y))
 
-window_width = 700
-window_height = 500
+  
+   ''' переміщає персонажа, застосовуючи поточну горизонтальну та вертикальну швидкість'''
+   def update(self):
+       # Спершу рух по горизонталі
+       self.rect.x += self.x_speed
+       self.rect.y += self.y_speed
+#Створюємо віконце
+win_width = 700
+win_height = 500
 display.set_caption("Лабіринт")
-window = display.set_mode((window_width, window_height))
-
+window = display.set_mode((win_width, win_height))
+back = (119, 210, 223) # задаємо колір відповідно до колірної схеми RGB
+#Створюємо стіни картинки
+w1 = GameSprite('assets/wall.png',win_width/2 - win_width/3, win_height/2, 300, 50)
+w2 = GameSprite('assets/wall.png', 370, 100, 50, 400)
+#створюємо спрайти
+packman = Player('assets/ghost.png', 5, win_height - 80, 80, 80, 0, 0)
+#ігровий цикл
 run = True
-
-wall_1 = GameSprite('assets/wall.png',80,180,200,250)
-
 while run:
-    for e in event.get():
-        window.fill((0, 200, 255))
-        if e.type == QUIT:
-            run = False
-            
-    wall_1.reset()
-    display.update()
+#цикл спрацьовує кожну 0.05 секунд
+   time.delay(50)
+   window.fill(back)#зафарбовуємо вікно кольором
+   for e in event.get():
+       if e.type == QUIT:
+           run = False
+       elif e.type == KEYDOWN:
+           if e.key == K_LEFT:
+               packman.x_speed = -5
+           elif e.key == K_RIGHT:
+               packman.x_speed = 5
+           elif e.key == K_UP:
+               packman.y_speed = -5
+           elif e.key == K_DOWN:
+               packman.y_speed = 5
+       elif e.type == KEYUP:
+           if e.key == K_LEFT:
+               packman.x_speed = 0
+           elif e.key == K_RIGHT:
+               packman.x_speed = 0
+           elif e.key == K_UP:
+               packman.y_speed = 0
+           elif e.key == K_DOWN:
+               packman.y_speed = 0
+#малюємо об'єкти
+   # w1.reset()
+   # w2.reset()
+   packman.reset()
+   #включаємо рух
+   packman.update()
+   display.update()
